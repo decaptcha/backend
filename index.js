db67ff55ad47c3c34c81df3a6c7514d6bcb2c1bd
Pranav Jain <pranavj1001@gmail.com>
2023-03-05 12:29:16 +0530

    declare new apis

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const path = require("path");
 
 const port = 3690;
-const storageRootFolder = path.join(__dirname, "../storage");
+const storageRootFolder = path.join(__dirname, "./storage");
 const SUCCESS_HTTP_CODE = 200;
 const SERVER_ERROR_CODE = 500;
 const BAD_REQ_ERROR_CODE = 400;
@@ -41,10 +41,69 @@ const getAndPrintErrorString = (url, error) => {
   return errorString;
 };
 
+/**
+ * Health Check API
+ */
 app.get("/", (req, res) => {
   res
     .status(SUCCESS_HTTP_CODE)
     .json({ ...successResponse, resp: "HEALTH OK!" });
+});
+
+/**
+ * API to load a captcha challenge page
+ * Response: {}
+ */
+app.get("/captcha", (req, res) => {
+  res.sendStatus(501);
+});
+
+/**
+ * API to publish the details of a captcha challenge page
+ * and returns if the challenge was passed/failed
+ * RequstBody: {}
+ * Response: {}
+ */
+app.post("/captcha", (req, res) => {
+  res.sendStatus(501);
+});
+
+/**
+ * API to load details of the projects
+ * if project_id is undefined then return minimal details of all projects of the user.
+ * if project_id is defined then return details of the selected project of the user.
+ * RequstBody: {}
+ * Response: {}
+ */
+app.get("/projects/:project_id", (req, res) => {
+  res.sendStatus(501);
+});
+
+/**
+ * API to publish details of a new projects
+ * RequstBody: {}
+ * Response: {}
+ */
+app.post("/project", (req, res) => {
+  res.sendStatus(501);
+});
+
+/**
+ * API to mark a project as inactive
+ * RequstBody: {}
+ * Response: {}
+ */
+app.delete("/project/:project_id", (req, res) => {
+  res.sendStatus(501);
+});
+
+/**
+ * API to upload images for a project
+ * RequstBody: {}
+ * Response: {}
+ */
+app.put("/upload_images/:project_id", (req, res) => {
+  res.sendStatus(501);
 });
 
 app.listen(port, () => {
